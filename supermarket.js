db63625e5e1385e2e3cf9c18a2960b0ed5c19ef4
vinhbachsy <bsdvinhuit@gmail.com>
2019-02-22 23:42:46 +0800

    Add sample supermarket locations

---
+++ supermarket.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        "address": "460 Alexandra Rd, #01-09 / #01-16, Singapore 119963",
+        "lat": 1.273910,
+        "long": 103.801370
+    },
+    {
+        "address": "Blk, 78A Telok Blangah Street 32, #01-01/02, Singapore 101078",
+        "lat": 1.273400,
+        "long": 103.808960
+    },
+    {
+        "address": "Blk 57 Dawson Rd, Dawson Place, #01-07, Singapore 142057",
+        "lat": 1.292710,
+        "long": 103.811000
+    },
+    {
+        "address": "20 Lengkok Bahru, #01-13, Singapore 159053",
+        "lat": 1.286120,
+        "long": 103.812170
+    },
+    {
+        "address": "Blk, 166 Bukit Merah Central, #01-3531, Singapore 150166",
+        "lat": 1.283090,
+        "long": 103.817670
+    },
+    {
+        "address": "Blk 108 Depot Rd #01-01 Depot Heights Shopping Centre, Singapore 100108",
+        "lat": 1.281200,
+        "long": 103.810270
+    }
+]
