03900d007e202da8f066a956387c35c67517110b
vinhbachsy <bsdvinhuit@gmail.com>
2019-02-23 00:51:57 +0800

    Add complete solution

--- index.js
+++ index.js
@@ -7,22 +7,30 @@ const supermarket = require('./supermarket')
 app.get('/', (req, res) => res.send('Hello world!'))
 
 app.get("/api/supermarket/nearby", (req, res) => {
-    const lat = req.params.lat
-    const long = req.params.long
-    
-    const distance = geolib.getDistance(
+    const lat = req.query.lat
+    const long = req.query.long
+
+    nearby = supermarket.reduce((prev, next) => 
+        calculateDistance(lat, long, prev) > calculateDistance(lat, long, next) ? 
+            next : 
+            prev)
+
+    res.json(
+        Object.assign({ distance: calculateDistance(lat, long, nearby) }, nearby))
+})
+
+app.listen(port, () => console.log(`Server started on ${port}`))
+
+function calculateDistance(lat, long, location) {
+    return geolib.getDistance(
         {
             latitude: lat, 
             longitude: long
         },
         {
-            latitude: supermarket[0].lat, 
-            longitude: supermarket[0].long
+            latitude: location.lat, 
+            longitude: location.long
         },
         10
     )
-
-    res.json({ distance: distance })
-})
-
-app.listen(port, () => console.log(`Server started on ${port}`))
+}
