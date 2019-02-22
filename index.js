a38644f29959c72fcc6bff6083030a7a8fe7872d
vinhbachsy <bsdvinhuit@gmail.com>
2019-02-23 00:07:33 +0800

    Add broken scenario

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express')
+const geolib = require('geolib')
 const app = express()
 const port = 3000
+const supermarket = require('./supermarket')
 
 app.get('/', (req, res) => res.send('Hello world!'))
+
+app.get("/api/supermarket/nearby", (req, res) => {
+    const lat = req.params.lat
+    const long = req.params.long
+    
+    const distance = geolib.getDistance(
+        {
+            latitude: lat, 
+            longitude: long
+        },
+        {
+            latitude: supermarket[0].lat, 
+            longitude: supermarket[0].long
+        },
+        10
+    )
+
+    res.json({ distance: distance })
+})
 
 app.listen(port, () => console.log(`Server started on ${port}`))
